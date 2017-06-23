f12da72a9de3991ca27a946aaf81b24ca7080908
EBone <enone_wxw@126.com>
2017-06-23 16:10:45 +0800

    have trouble in writing intto files

--- Convert24BitWave.py
+++ Convert24BitWave.py
@@ -1,5 +1,5 @@
 import struct
-datalike=b'\x00\x00\x02\xff\xff\xff'	#b'\xff\xff\xff\xff\xff\xff'
+#datalike=b'\x00\x00\x02\xff\xff\xff'	#b'\xff\xff\xff\xff\xff\xff'
 
 
 class Handle_24bit_Data:
@@ -9,34 +9,38 @@ class Handle_24bit_Data:
 		self.handle_method=handle_method
 
 	def iter_bytes_data(self):
-		datalist=struct.iter_unpack('>BH',self.bytes_data)
+		datalist=list(struct.iter_unpack('<BH',self.bytes_data))
 		return datalist
 		
 	def make_int24_value(self,data_tp):
-		str_data=''.join([hex(data_tp[0]),hex(data_tp[1])[2:]])
-		print(str_data)
+		low_int=data_tp[1]
+		low_pos=hex(low_int)[2:]
+		low_pos_len=len(low_pos)
+		if low_pos_len<4:
+			temp=low_pos[:low_pos_len]
+			low_pos=(4-low_pos_len)*'0'+temp
+
+		str_data=''.join([hex(data_tp[0]),low_pos])
 		hex_value =int(str_data,base=16)
 		return hex_value
 
 	def split_int24(self,hex_num):
 		hex_str_nbytes=hex(hex_num)
-		print(hex_str_nbytes)
 		if len(hex_str_nbytes)>6:	
 			hex_str_1bytes=hex_str_nbytes[:-4]
-			print(hex_str_1bytes)
 			hex_str_2bytes=hex_str_nbytes[-4:]
 			return int(hex_str_1bytes,base=16),int(hex_str_2bytes,base=16)
 		else:
 			return 0,int(hex_str_nbytes,base=16)
 	
 	def pack_data(self,tp):
-		return struct.pack('>BH',*tp)
+		return struct.pack('<BH',*tp)
 	
 	def __call__(self):
 		data_generator=self.iter_bytes_data()
-		int24_datalist=map(self.make_int24_value,data_generator)			#get int value list
+		int24_datalist=list(map(self.make_int24_value,data_generator))			#get int value list
 		handled_data=map(self.handle_method,int24_datalist)					#handle int data
-		hex_tuple_list=map(self.split_int24,handled_data)					#split to 8bit and 16bit
+		hex_tuple_list=list(map(self.split_int24,handled_data))			#split to 8bit and 16bit
 		int24_bitdata=map(self.pack_data,hex_tuple_list)
 		
 		return int24_bitdata
@@ -44,7 +48,8 @@ class Handle_24bit_Data:
 
 if __name__=="__main__":
 	def halflower(data):
-		return int(data/3) 
+		return int(data)
+	datalike=b'\x93\x11\x00'
 		
 	print(list(Handle_24bit_Data(datalike,halflower)()))
 	
